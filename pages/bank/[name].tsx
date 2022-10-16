6f16f0fc652828c17e81a9f89c31c563624fb81c
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-10-16 17:58:54 +0100

    expand image on click

--- pages/bank/[name].tsx
+++ pages/bank/[name].tsx
@@ -1,8 +1,9 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Button, Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import { GetStaticProps } from "next";
 import { fetchFromEndpoint, getAllMushroomNames } from "../../utils/server";
-import { useRouter } from "next/router";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export async function getStaticPaths() {
   const paths = getAllMushroomNames();
@@ -23,27 +24,60 @@ export const getStaticProps: GetStaticProps = async (context) => {
   return {
     props: {
       mushroomSrcList,
+      mushroomName,
     },
   };
 };
 
 const InfoBank = ({
   mushroomSrcList,
+  mushroomName,
 }: {
   mushroomSrcList: string[] | undefined;
+  mushroomName: string;
 }) => {
+  const [expandIndex, setExpandIndex] = useState<number | null>(null);
+  const visibleMushrooms = mushroomSrcList?.filter(
+    (_, index) => index === expandIndex || expandIndex === null
+  );
+
+  console.log(expandIndex);
   return (
-    <SimpleGrid columns={4}>
-      {mushroomSrcList?.map((imgSrc) => (
-        <Image
-          key={imgSrc}
-          src={imgSrc}
-          alt="mushroom image"
-          width={200}
-          height={200}
-        />
-      ))}
-    </SimpleGrid>
+    <Container mt={5}>
+      <Heading>{mushroomName} mushroom</Heading>
+      <Link href={`https://www.wildfooduk.com/mushroom-guide/${mushroomName}`}>
+        More Info
+      </Link>
+      {expandIndex === null && (
+        <SimpleGrid columns={4} gap={1}>
+          {visibleMushrooms?.map((imgSrc, index) => (
+            <Image
+              key={imgSrc}
+              src={imgSrc}
+              alt="mushroom image"
+              width={200}
+              height={200}
+              onClick={() => setExpandIndex(index)}
+              style={{ cursor: "pointer" }}
+            />
+          ))}
+        </SimpleGrid>
+      )}
+      {expandIndex !== null && (
+        <Container>
+          <Button onClick={() => setExpandIndex(null)}>View all</Button>
+          {visibleMushrooms?.map((imgSrc) => (
+            <Image
+              key={imgSrc}
+              src={imgSrc}
+              alt="mushroom image"
+              width={1000}
+              height={1000}
+            />
+          ))}
+        </Container>
+      )}
+    </Container>
   );
 };
 
