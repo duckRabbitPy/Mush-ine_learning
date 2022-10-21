0ee8d160b1462171ed521c5d76858aabbe616c46
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-10-22 00:32:50 +0100

    use handler pattern

--- pages/forageTrpc.tsx
+++ pages/forageTrpc.tsx
@@ -1,17 +1,41 @@
-import { Button, Container, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  Spinner,
+} from "@chakra-ui/react";
 import Image from "next/image";
-import HomeBtn from "./components/HomeBtn";
 import { useState } from "react";
+import { TestMushroom } from "../utils/server";
 import { trpc } from "../utils/trpc";
+import HomeBtn from "./components/HomeBtn";
 
 const Forage = () => {
-  const testMushrooms = trpc.testMushrooms.useQuery({
-    omitArr: [],
-    number: 5,
-  }).data;
-
+  const [testMushrooms, setTestMushrooms] = useState<TestMushroom[] | []>([]);
+  const [omitArr, setOmitArr] = useState<string[]>([]);
   const [inputAnswer, setInputAnswer] = useState<string | null>(null);
   const correctMushroom = testMushrooms?.filter((t) => t.correctMatch)[0];
+
+  const getTestMushrooms = trpc.testMushrooms.useQuery({
+    omitArr,
+    number: 5,
+  });
+
+  const handleNextBtn = async () => {
+    const newTestMushrooms = getTestMushrooms.data;
+    if (newTestMushrooms) {
+      setTestMushrooms(newTestMushrooms);
+      setOmitArr((arr) => {
+        if (correctMushroom) {
+          arr.push(correctMushroom.name);
+        }
+        return arr;
+      });
+    }
+  };
+
   return (
     <Container>
       <HomeBtn />
@@ -20,29 +44,33 @@ const Forage = () => {
         {inputAnswer === correctMushroom?.name && "✅"}
         {inputAnswer && inputAnswer !== correctMushroom?.name && "❌"}
       </Heading>
-      <Button onClick={() => {}}>Start</Button>
-      <SimpleGrid columns={2} gap={2}>
-        {testMushrooms?.map((testMushroom) => {
-          return (
-            <Container key={testMushroom.name}>
-              <Image
-                onClick={() => {
-                  setInputAnswer(testMushroom.name);
-                }}
-                src={testMushroom.src}
-                alt="testMushroom"
-                height={200}
-                width={200}
-                style={{
-                  opacity:
-                    inputAnswer && !testMushroom.correctMatch ? "0.5" : 1,
-                }}
-              />
-              <Text>{inputAnswer ? testMushroom.name : ""}</Text>
-            </Container>
-          );
-        })}
-      </SimpleGrid>
+      <Button onClick={handleNextBtn}>Start</Button>
+      {getTestMushrooms.isLoading ? (
+        <Spinner />
+      ) : (
+        <SimpleGrid columns={2} gap={2}>
+          {testMushrooms?.map((testMushroom) => {
+            return (
+              <Container key={testMushroom.name}>
+                <Image
+                  onClick={() => {
+                    setInputAnswer(testMushroom.name);
+                  }}
+                  src={testMushroom.src}
+                  alt="testMushroom"
+                  height={200}
+                  width={200}
+                  style={{
+                    opacity:
+                      inputAnswer && !testMushroom.correctMatch ? "0.5" : 1,
+                  }}
+                />
+                <Text>{inputAnswer ? testMushroom.name : ""}</Text>
+              </Container>
+            );
+          })}
+        </SimpleGrid>
+      )}
     </Container>
   );
 };
