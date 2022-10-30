3a156907a23aa0a9e2a677443d8d8217a52b32ce
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-10-30 14:04:23 +0000

    load env var for migration script

--- server/database/migrations/migration-1.ts
+++ server/database/migrations/migration-1.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 import { randomUUID } from "crypto";
 import { getCloudMushrooms } from "../../../utils/server";
 import db from "../connection";
@@ -7,15 +9,12 @@ export async function initTables() {
   await db.query(
     "CREATE TABLE mushine_learning_user (id SERIAL PRIMARY KEY, user_id VARCHAR(50),xp INTEGER)"
   );
-
   await db.query(
     "CREATE TABLE mushine_training_mushrooms (id SERIAL PRIMARY KEY, name VARCHAR (50), mushroom_id UUID)"
   );
-
   await db.query(
     "CREATE TABLE mushine_training_weightings (id SERIAL PRIMARY KEY, user_id VARCHAR (50), correct_mushroom VARCHAR (50), misidentified_as VARCHAR (50), weight integer, timestamp TIMESTAMP)"
   );
-
   await db.query(
     "CREATE TABLE mushine_level_snapshots (level SERIAL PRIMARY KEY,user_id VARCHAR (50),snapshot JSONB);"
   );
